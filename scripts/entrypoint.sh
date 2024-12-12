1e971d38d702d94aad547c89f3d291228af9e55c
TICO-79009 <takashi.matsuda@mail.toyota-shokki.co.jp>
2024-12-12 07:35:43 +0000

    Change with the upgrade to 6.1.1

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,8 +3,11 @@
 OWN_SCRIPT_NAME=`basename $0`
 LOG_SCRIPT_PATH="/node-red/shells/outputlog.sh"
 
+# バージョン表示
+cat application.info
+
 # 必要なファイル生成
-/node-red/shells/startup_preprocess.sh
+/node-red/shells/entrypoint_preprocess.sh
 
 if [ $? -ne 0 ]; then
     ${LOG_SCRIPT_PATH} "${OWN_SCRIPT_NAME}" "error" "Creating setting file failed."
@@ -15,8 +18,16 @@ fi
 # ユーザデータの復元
 /node-red/shells/restore_userdata.sh
 
-# Python仮想環境起動
-source ./venv/bin/activate
+# NodeRED実行
+trap stop SIGINT SIGTERM
 
-# dockerのエントリーポイント実行
-exec ./entrypoint.sh "$@"
+function stop() {
+	kill $CHILD_PID
+	wait $CHILD_PID
+}
+
+exec /usr/local/bin/node /usr/src/node-red/packages/node_modules/node-red/red.js --userDir /data $FLOWS --settings /node-red/settings/settings.js "${@}" &
+
+CHILD_PID="$!"
+
+wait "${CHILD_PID}"
